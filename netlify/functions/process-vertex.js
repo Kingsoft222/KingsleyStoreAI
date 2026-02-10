6c3e29eca849a12514b515c3ee118b731d6b4c4b
Kingsoft222 <kman39980@gmail.com>
2026-02-10 18:56:07 +0100

    Reset: Stripped masking to fix silent failure

--- netlify/functions/process-vertex.js
+++ netlify/functions/process-vertex.js
@@ -2,16 +2,11 @@ const { GoogleAuth } = require('google-auth-library');
 const axios = require('axios');
 
 exports.handler = async (event) => {
-    const headers = { 
-        'Content-Type': 'application/json', 
-        'Access-Control-Allow-Origin': '*' 
-    };
-
+    const headers = { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' };
     try {
         const body = JSON.parse(event.body);
-        // Supports both 'image' and 'face' keys just in case
-        const imageData = body.image || body.face;
-        const clothName = body.cloth;
+        const image = body.image || body.face;
+        const cloth = body.cloth;
 
         const auth = new GoogleAuth({
             credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY),
@@ -20,19 +15,21 @@ exports.handler = async (event) => {
         const client = await auth.getClient();
         const token = (await client.getAccessToken()).token;
 
-        // Using the most stable legacy model for 2026
         const apiURL = `https://us-central1-aiplatform.googleapis.com/v1/projects/${process.env.GOOGLE_PROJECT_ID}/locations/us-central1/publishers/google/models/image-generation@006:predict`;
 
+        /**
+         * 2026 BRUTE FORCE:
+         * We stop trying to "Inpaint" and instead use "Image-to-Image"
+         * with a very high prompt weight to force the change.
+         */
         const payload = {
             instances: [{
-                prompt: `A high-quality fashion photo of the person wearing a ${clothName} senator native outfit. Professional studio lighting.`,
-                image: { 
-                    bytesBase64Encoded: imageData.split(';base64,').pop() 
-                }
+                prompt: `A high-quality fashion photo. Replace the current clothes with a luxury ${cloth} senator native outfit. Maintain the person's face and pose.`,
+                image: { bytesBase64Encoded: image.split(';base64,').pop() }
             }],
             parameters: {
                 sampleCount: 1,
-                // We are removing all complex mask/edit keys that are causing the 59ms rejection
+                // We remove editMode and maskConfig to stop the 'Silent Success' failure
                 safetySetting: "block_only_high",
                 personGeneration: "allow_adult"
             }
@@ -54,7 +51,7 @@ exports.handler = async (event) => {
         };
 
     } catch (error) {
-        console.error("LEGACY_RESET_ERROR:", error.message);
+        console.error("DEBUG_FAIL:", error.message);
         return {
             statusCode: 500,
             headers,
